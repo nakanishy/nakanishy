f4acb529e7e8c91b9a4bc57954d3022587995454
Keita Nakanishi <nakanishy@yahoo.com>
2021-08-19 10:35:25 +0900

    Remove unused variables

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,23 +1,5 @@
-/* https://palx.jxnblk.com/2D7CC9 */
-
-export const colorPalette = {
-  white: '#fff',
-  gray0: '#f9f9f9',
-  gray1: '#ebedef',
-  gray2: '#dde1e4',
-  gray3: '#ced3d8',
-  gray4: '#bdc4cb',
-  gray5: '#abb3bc',
-  gray6: '#96a1ab',
-  gray7: '#7d8a97',
-  gray8: '#606d7a',
-  gray9: '#384047',
-}
-
 export const fontFamily = {
   sansSerif: 'Montserrat, "Helvetica Neue", "Hiragino Sans", sans-serif',
-  monospace:
-    '"Fira Mono", Consolas, Monaco, Menlo, Courier, "Ubuntu Mono", monospace',
 }
 
 export const fontSize = {
@@ -63,32 +45,8 @@ export const borderRadius = {
   xl: 32,
 }
 
-export const shadow = {
-  none: `0 0px 0px rgba(0, 0, 0, 0), 0 0px 0px rgba(0, 0, 0, 0)`,
-  s: `0 4px 4px rgba(0, 0, 0, 0.04), 0 4px 8px rgba(0, 0, 0, 0.02)`,
-  m: `0 8px 8px rgba(0, 0, 0, 0.04), 0 8px 16px rgba(0, 0, 0, 0.03)`,
-  l: `0 16px 16px rgba(0, 0 ,0, 0.04), 0 16px 32px rgba(0, 0, 0, 0.04)`,
-}
-
 export const media = {
   narrowPc: '(max-width: 1000px)',
   tablet: '(max-width: 768px)',
   sp: '(max-width: 480px)',
 }
-
-export const zIndex = {
-  colorTooltip: 500,
-  tooltip: 500,
-  dropdown: 1200,
-  detailHeader: 1000,
-  picker: 2000,
-  header: 1000,
-  sidebar: 1000,
-  colorPalette: 1100,
-  modal: 10000,
-  toaster: 20000,
-}
-
-export const collapsedSidebarWidth = 75
-export const sidebarWidth = 314
-export const selectHeight = 46
